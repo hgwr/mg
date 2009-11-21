048be4979670b6d97c1c659209fbdd3dd4602c8f
Shigeru Hagiwara <hgwrsgr@gmail.com>
2009-11-21 22:11:48 +0900

    initial version

---
+++ background.js
@@ -0,0 +1,73 @@
+/*
+ * 2009-11-21
+ * This code is based on ChromeGuestures
+ * http://ss-o.net/chrome_extension/
+ * http://bitbucket.org/os0x/chromeguestures/
+ */
+
+function switchToRightTab(tab) {
+  chrome.tabs.getAllInWindow(tab.windowId,
+                             function (tabs) {
+                               tabs.forEach(function (_t, i) {
+                                              if (_t.id === tab.id) {
+                                                var newtab = tabs[i + 1] || tabs[0];
+                                                if (newtab) {
+                                                  chrome.tabs.update(newtab.id, { selected: true });
+                                                  chrome.tabs.sendRequest(tab.id, {action: "remove_gesture_listener"});
+                                                  chrome.tabs.sendRequest(newtab.id, {action: "right_button_down"});
+                                                }
+                                              }
+                                            });
+                             });
+}
+
+function switchToLeftTab(tab) {
+  chrome.tabs.getAllInWindow(tab.windowId,
+                             function (tabs) {
+                               tabs.forEach(function (_t, i) {
+                                              if (_t.id === tab.id) {
+                                                var newtab = tabs[i - 1] || tabs[tabs.length - 1];
+                                                if (newtab) {
+                                                  chrome.tabs.update(newtab.id, { selected: true });
+                                                  chrome.tabs.sendRequest(tab.id, {action: "remove_gesture_listener"});
+                                                  chrome.tabs.sendRequest(newtab.id, {action: "right_button_down"});
+                                                }
+                                              }
+                                            });
+                             });
+}
+
+function removeGestureListener() {
+  chrome.tabs.getSelected(null,
+                          function(tab) {
+                            chrome.tabs.sendRequest(tab.id, {action: "remove_gesture_listener"});
+                          });
+}
+
+function newWindow() {
+  chrome.windows.create({ url: "http://www.google.com/" });
+}
+
+function messageDispatcher(request, sender) {
+  switch (request.action) {
+  case "right_tab":
+    switchToRightTab(sender.tab);
+    break;
+  case "left_tab":
+    switchToLeftTab(sender.tab);
+    break;
+  case "remove_gesture_listener":
+    removeGestureListener();
+    break;
+  case "new_window":
+    newWindow();
+    break;
+  }
+}
+
+window.addEventListener("load", function () {
+                          chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+                                                                   sendResponse({});
+                                                                   messageDispatcher(request, sender);
+                                                                 });
+                        });
---
+++ mg.js
@@ -0,0 +1,190 @@
+/*
+ * 2009-11-21
+ * This code is based on chromegestures (http://code.google.com/p/chromegestures/).
+ */
+
+var MgActions = {
+  newWindow: function() {
+    chrome.extension.sendRequest({action: "new_window"});
+  },
+  newTab: function() {
+    window.open("http://www.google.com");
+  },
+  blankTab: function() {
+    window.open("about:blank");
+  },
+  closeTab: function() {
+    window.open("", "_self");
+    self.close();
+  },
+  historyBack: function() {
+    history.back();
+  },
+  historyForward: function() {
+    history.forward();
+  },
+  reload: function() {
+    location.reload();
+  },
+  rightTab: function() {
+    chrome.extension.sendRequest({action: "right_tab"});
+  },
+  leftTab: function() {
+    chrome.extension.sendRequest({action: "left_tab"});
+  }
+};
+
+var Mg = {
+  gestureDefs: {
+    wheelDown: MgActions.rightTab,
+    wheelUp: MgActions.leftTab,
+    "U": MgActions.newTab,
+    "D": MgActions.newWindow,
+    "L": MgActions.historyBack,
+    "R": MgActions.historyForward,
+    "3,1": MgActions.historyBack,
+    "1,3": MgActions.historyForward,
+    "U,L": MgActions.leftTab,
+    "U,R": MgActions.rightTab,
+    "D,R": MgActions.closeTab,
+    "U,D": MgActions.reload
+  },
+  thresholds: {
+    click: 1000,
+    time: 30,
+    squareOfDistance: 50
+  },
+  pb: null,  // Type of the butotn pressed before
+  px: 0,
+  py: 0,
+  past: new Date(),
+  gesture: [],
+  clickGesturing: false,
+
+  determineMotion: function (dx, dy) {
+    var abs = {
+      dx: Math.abs(dx),
+      dy: Math.abs(dy)
+    };
+    if ((abs.dx * abs.dx + abs.dy * abs.dy) < Mg.thresholds.squareOfDistance) {
+      return null;
+    } else {
+      document.oncontextmenu = function () { return false; };
+    }
+    if (abs.dy > abs.dx) {
+      return dy < 0 ? "U" : "D";
+    } else {
+      return dx > 0 ? "R" : "L";
+    }
+  },
+
+  mousemove: function(e) {
+    var now = new Date(),
+    x = e.clientX,
+    y = e.clientY,
+    dt = now.getTime() - Mg.past.getTime(),
+    dx, dy, motion;
+    if (dt < Mg.thresholds.time) {
+      return;
+    } else {
+      dx = x - Mg.px;
+      dy = y - Mg.py;
+      Mg.px = x;
+      Mg.py = y;
+      motion = Mg.determineMotion(dx, dy);
+      if (motion && motion != Mg.gesture[Mg.gesture.length - 1]) {
+        if (Mg.gesture.length > 4) {
+          Mg.gesture = [motion];
+        } else {
+          Mg.gesture.push(motion);
+        }
+        console.log(Mg.gesture.toString());
+      }
+      Mg.past = now;
+    }
+  },
+
+  mousescroll: function(e) {
+    document.oncontextmenu = function () { return false; };
+    event.preventDefault();
+    if (e.wheelDelta < 0) {
+      Mg.gestureDefs.wheelDown();
+    } else {
+      Mg.gestureDefs.wheelUp();
+    }
+    return false;
+  },
+
+  mousedown: function(e) {
+    var now = new Date(),
+    dt = now.getTime() - Mg.past.getTime();
+
+    if (e.which == 3) {
+      document.oncontextmenu = function () { return true; };
+      Mg.px = e.clientX;
+      Mg.py = e.clientY;
+      window.addEventListener("mousemove", Mg.mousemove);
+      window.addEventListener("mousewheel", Mg.mousescroll);
+    }
+
+    if (dt < Mg.thresholds.click && Mg.pb != e.which) {
+      Mg.gesture = [Mg.pb, e.which];
+      Mg.clickGesturing = true;
+    }
+    Mg.pb = e.which;
+    Mg.past = now;
+  },
+
+  removeGestureListener: function () {
+    window.removeEventListener("mousemove", Mg.mousemove);
+    window.removeEventListener("mousewheel", Mg.mousescroll);
+    Mg.gesture = [];
+  },
+
+  mouseup: function(e) {
+    var handler;
+
+    if (e.which == 3) {
+      e.preventDefault();     // TODO: 調べる
+      e.stopPropagation();
+      chrome.extension.sendRequest({action: "remove_gesture_listener"});
+      handler = Mg.gestureDefs[Mg.gesture];
+      Mg.removeGestureListener();
+      if (handler) { handler(); }
+    }
+
+    if (Mg.clickGesturing) {
+      document.oncontextmenu = function () { return false; };
+      e.preventDefault();     // TODO: 調べる
+      e.stopPropagation();
+      Mg.clickGesturing = false;
+      console.log("click " + Mg.gesture.toString());
+    }
+  },
+
+  simulateMouseEvent: function (eventName, button) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent(eventName, true, true,
+                       window, 0, 0, 0, 0, 0,
+                       false, false, false, false, button, null);
+    document.documentElement.dispatchEvent(evt);
+  },
+
+  requestHandler: function(request, sender, sendResponse) {
+    sendResponse({});
+    switch (request.action) {
+    case "right_button_down":
+      Mg.simulateMouseEvent("mousedown", 2);
+      break;
+    case "remove_gesture_listener":
+      Mg.removeGestureListener();
+      break;
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+                            window.addEventListener("mousedown", Mg.mousedown, false);
+                            window.addEventListener("mouseup", Mg.mouseup, false);
+                            chrome.extension.onRequest.addListener(Mg.requestHandler);
+                          });
